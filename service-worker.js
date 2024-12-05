0fb5801477451cd25f87d234f515371619ab6eb4
HeroBuxx <me@herobuxx.me>
2024-12-05 22:57:38 +0700

    init notif test
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+// service-worker.js
+self.addEventListener('push', (event) => {
+    console.log('Push event received:', event);
+    const data = event.data.json();
+  
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icon.png',
+    });
+  });  
